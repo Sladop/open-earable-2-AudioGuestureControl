9c446a2cc1166ee0a445f36caedde681d7c407e3
Philipp Ochs <ufiiu@student.kit.edu>
2026-02-15 01:34:27 +0100

    processing utils doc & finalized

--- src/micropython/Processing Graph/processing_utils.py
+++ src/micropython/Processing Graph/processing_utils.py
@@ -12,15 +12,45 @@ class ParseType(Enum):
     FLOAT = 7
     DOUBLE = 8
 
-class SensorData:
 
+class SensorData:
+    """
+   SensorValue Struct
+
+    Attributes:
+        data (bytes): Rohdaten
+        timestamp (int): Zeitstempel
+    """
     def __init__(self, data: bytes, timestamp: int):
         self.data = data
         self.timestamp = timestamp
-        self.size = len(data)
 
 
 def decode_sensor_data(sample: SensorData, parse_type: ParseType):
+    """
+        Decodes raw sensor data into the  numerical val.
+
+        Interprets the bytes from `SensorData.data` according to the given
+        `parse_type` and returns the value as int or float.
+
+        Args:
+            sample (SensorData): The sensor data object containing the raw bytes (`data`) and timestamp.
+            parse_type (ParseType): The data type of the raw bytes. Supported types:
+                - ParseType.UINT8
+                - ParseType.INT8
+                - ParseType.UINT16
+                - ParseType.INT16
+                - ParseType.UINT32
+                - ParseType.INT32
+                - ParseType.FLOAT
+                - ParseType.DOUBLE
+
+        Returns:
+            int, float: Decoded numerical value of the sensor.
+
+        Raises:
+            ValueError: If an unsupported `parse_type` is entered.
+    """
     data = sample.data
     if parse_type == ParseType.UINT8:
         return data[0]
@@ -39,29 +69,23 @@ def decode_sensor_data(sample: SensorData, parse_type: ParseType):
     elif parse_type == ParseType.DOUBLE:
         return struct.unpack("<d", data)[0]
     else:
-        raise ValueError("Unsupported parse type")
-
-def decode_as_float(parse_type: ParseType, data: bytes):
-    if parse_type == ParseType.UINT8:
-        return float(data[0])
-    elif parse_type == ParseType.INT8:
-        return float(struct.unpack("<b", data)[0])
-    elif parse_type == ParseType.UINT16:
-        return float(struct.unpack("<H", data)[0])
-    elif parse_type == ParseType.INT16:
-        return float(struct.unpack("<h", data)[0])
-    elif parse_type == ParseType.UINT32:
-        return float(struct.unpack("<I", data)[0])
-    elif parse_type == ParseType.INT32:
-        return float(struct.unpack("<i", data)[0])
-    elif parse_type == ParseType.FLOAT:
-        return struct.unpack("<f", data)[0]
-    elif parse_type == ParseType.DOUBLE:
-        return struct.unpack("<d", data)[0]
-    else:
-        return 0.0
+        raise ValueError(f"Unsupported parse_type {parse_type}")
 
 def check_timestamp_sync(inputs, in_ports: int, threshold_us: int):
+    """
+        Check if multiple sensor samples are synchronized within a given threshold.
+
+        Compares the timestamps of multiple sensor inputs and returns True
+        if all timestamps are within `threshold_us` of the first sample's timestamp.
+
+        Args:
+            inputs (list of SensorData): List of sensor data objects
+            in_ports (int): Number of inputs.
+            threshold_us (int): Maximum allowed difference in time.
+
+        Returns:
+            bool: True if all timestamps are within the threshold, False otherwise.
+        """
     ref_ts = inputs[0].timestamp
     for i in range(1, in_ports):
         ts = inputs[i].timestamp
